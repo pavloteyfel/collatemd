2a9c0417b180f70d76cc38f2d3dc4fdba2da827d
Pasha Taylor <pavloteyfel@gmail.com>
2025-04-08 16:28:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='collate',
     version='1.0',
-    py_modules=['collate'],
+    py_modules=['collatemd'],
     install_requires=[
         # list dependencies here if needed, e.g.,
         # 'requests',
     ],
     entry_points={
         'console_scripts': [
-            'collate = collate:main',
+            'collate = collatemd:main',
         ],
     }
 )
